d8febb83c7097469fa590216cbb9ee7aa19caab5
anushareddy81 <anusha22naveen@gmail.com>
2019-02-05 16:58:33 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Anusha' ,address: 'knr'},
+  { id: 12, name: 'Arjun',address: 'knr' },
+  { id: 13, name: 'shiva',address: 'knr'  },
+  { id: 14, name: 'dhruthi',address: 'knr'  },
+  { id: 15, name: 'Magneta' ,address: 'knr' },
+  { id: 16, name: 'RubberMan' ,address: 'knr' },
+  { id: 17, name: 'Dynama',address: 'knr'  },
+  { id: 18, name: 'Dr IQ',address: 'knr'  },
+  { id: 19, name: 'Magma',address: 'knr'  },
+  { id: 20, name: 'Tornado',address: 'knr' }
+];
